7fcf13dd0f06383590d4c27937d9b781c085220a
Emily S <emily.sugano@gmail.com>
2018-06-04 21:17:40 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,7 +11,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 # FROM projects
 # JOIN pledges ON
 # id = pledges.project_id;"
-"SELECT SUM(amount) FROM pledges WHERE project_id = 4 ;"
+"SELECT SUM(amount) FROM pledges WHERE project_id = 4;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
