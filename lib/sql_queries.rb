d5dec6952051b68686de4b9204e3e0fe541288da
Emily S <emily.sugano@gmail.com>
2018-06-04 21:57:03 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -39,7 +39,6 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
   INNER JOIN pledges ON
   users.id = pledges.user_id
   GROUP BY users.name
-  GROUP BY users.name
   ORDER BY total;"
 end
 
